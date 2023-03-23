0a4e0b003eae85284df52af11c63b809aee8c928
trac0082 <aidantracy@outlook.com>
2023-03-23 16:40:02 -0600

    Implements task #1

--- operations.js
+++ operations.js
@@ -3,8 +3,14 @@ function sum(a, b)
     return a + b;
 }
 
+function subtract(a, b)
+{
+    return a - b;
+}
+
 
 module.exports = {
     sum,
-    //subtract, //TODO: uncomment this line to make the subtract function available in other files
+    subtract
+     //TODO: uncomment this line to make the subtract function available in other files
 };
--- operations.test.js
+++ operations.test.js
@@ -2,8 +2,9 @@
 const {sum} = require('./operations')
 
 //TODO: uncomment the next line
-//const {subtract} = require('./operations')
+const {subtract} = require('./operations')
 
+// Sum Test Cases
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3)
 });
@@ -14,4 +15,18 @@ test('adds 0 + 4 to equal 4', () => {
 
 test('adds -1 + (-7) to equal -8', () => {
     expect(sum(-1, -7)).toBe(-8)
+});
+
+// Subtract Test Cases
+
+test('subtracts 1 - 2 to equal -1', () => {
+    expect(subtract(1, 2)).toBe(-1)
+});
+
+test('subtracts 4 - 2 to equal 2', () => {
+    expect(subtract(4, 2)).toBe(2)
+});
+
+test('subtracts -1 - (-7) to equal 6', () => {
+    expect(subtract(-1, -7)).toBe(6)
 });
