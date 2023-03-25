958e0b30786548a26f01b7061cfc1e6eb1a0d6be
trac0082 <aidantracy@outlook.com>
2023-03-25 16:19:05 -0600

    Closes Task #4

--- app.test.js
+++ app.test.js
@@ -24,6 +24,7 @@ test('GET a route that does not exist and expect a 404 (Not Found) status code',
     await request.get('/route_that_does_not_exist').expect(404)
 });
 
+// Sum route tests
 test('GET the sum api with the valid parameters and expect the correct 200 status code', async () => {
     await request.get('/api/operations/sum/3/5').expect(200)
 });
@@ -42,6 +43,27 @@ test('GET the sum api with the a valid first parameter and an invalid second par
 test('GET the sum api with the a valid first parameter and a missing second parameter and expect the 404 (not found) status code, because that route does not exist', async () => {
     await request.get('/api/operations/sum/3/').expect(404)
     await request.get('/api/operations/sum/3').expect(404)
+});
+
+// Subtraction route tests
+test('GET the subtract api with the valid parameters and expect the correct 200 status code', async () => {
+    await request.get('/api/operations/subtract/5/3').expect(200)
+});
+
+test('GET the subtract api with the valid parameters (5,3) and expect the correct result returned (2)', async () => {
+    const response = await request.get('/api/operations/subtract/5/3')
+    console.log(`response.text = ${response.text}`);
+
+    expect(Number(response.text)).toBe(2)
+});
+
+test('GET the subtract api with the a valid first parameter and an invalid second parameter and expect the 400 (bad request) status code', async () => {
+    await request.get('/api/operations/subtract/3/second_parameter_is_not_a_number').expect(400)
+});
+
+test('GET the subtract api with the a valid first parameter and a missing second parameter and expect the 404 (not found) status code, because that route does not exist', async () => {
+    await request.get('/api/operations/subtract/3/').expect(404)
+    await request.get('/api/operations/subtract/3').expect(404)
 });
 
 test('should be listening', async () => {
