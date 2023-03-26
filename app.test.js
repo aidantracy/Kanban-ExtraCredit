74f445c65c68c35d3ee7e6ec33ec56f562d8e21c
trac0082 <aidantracy@outlook.com>
2023-03-26 07:57:32 -0600

    Closes #6

--- app.test.js
+++ app.test.js
@@ -66,6 +66,27 @@ test('GET the subtract api with the a valid first parameter and a missing second
     await request.get('/api/operations/subtract/3').expect(404)
 });
 
+// Divide route tests
+test('GET the divide api with the valid parameters and expect the correct 200 status code', async () => {
+    await request.get('/api/operations/divide/6/3').expect(200)
+});
+
+test('GET the divide api with the valid parameters (6,3) and expect the correct result returned (2)', async () => {
+    const response = await request.get('/api/operations/divide/6/3')
+    console.log(`response.text = ${response.text}`);
+
+    expect(Number(response.text)).toBe(2)
+});
+
+test('GET the divide api with the a valid first parameter and an invalid second parameter and expect the 400 (bad request) status code', async () => {
+    await request.get('/api/operations/divide/3/second_parameter_is_not_a_number').expect(400)
+});
+
+test('GET the divide api with the a valid first parameter and a missing second parameter and expect the 404 (not found) status code, because that route does not exist', async () => {
+    await request.get('/api/operations/divide/3/').expect(404)
+    await request.get('/api/operations/divide/3').expect(404)
+});
+
 test('should be listening', async () => {
     await request.get('/').expect(200);
 });
